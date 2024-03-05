4247efdea195d07fce4d0bc18062697015afa5ec
InteractiveOfCelcius <134863563+InteractiveOfCelcius@users.noreply.github.com>
2024-03-05 18:14:48 +0100

    Update porotical.py

--- porotical.py
+++ porotical.py
@@ -1,7 +1,9 @@
+## Importing libraries.
 utils = Utils()
 sound = Sound()
 os = OS()
 
+## Testing some functions
 utils.console.info('Hi', 'there')
 utils.console.warn('This is a warning')
 utils.console.error('This is an error')
@@ -10,6 +12,6 @@ r = requests.get('http://example.com/')
 utils.console.info('Status code from example.com is:', r.status_code)
 utils.console.info('The current path is', os.path())
 
-
+## Trying to play some random sound in my computer.
 utils.console.info('Current XGIN Executor version is', version)
 sound.playSound(f'{os.path()}/bloxxonova6.mp3')
