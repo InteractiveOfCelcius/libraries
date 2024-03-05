0d6624c69450a26b3defeacf25b6521a5a9a3bf7
InteractiveOfCelcius <134863563+InteractiveOfCelcius@users.noreply.github.com>
2024-03-05 17:30:36 +0100

    Update porotical.py

--- porotical.py
+++ porotical.py
@@ -1,4 +1,5 @@
 utils = Utils()
+sound = Sound()
 
 utils.console.info('Hi', 'there')
 utils.console.warn('This is a warning')
@@ -6,3 +7,6 @@ utils.console.error('This is an error')
 wait(2)
 r = requests.get('http://example.com/')
 utils.console.info('Status code from example.com is:', r.status_code)
+
+
+sound.playSound('/bloxxonova6.mp3')
